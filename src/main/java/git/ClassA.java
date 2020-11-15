5c7f02f0b6c7ab6a05ac912184382ea4afa7c2fe
Iulian86 <iuliandiaconescu86@gmail.com>
2020-11-15 13:09:18 +0200

    add new feature (#1)

--- src/main/java/git/ClassA.java
+++ src/main/java/git/ClassA.java
@@ -3,6 +3,9 @@ package git;
 public class ClassA {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        ClassB classB = new ClassB();
+        classB.sayHi("Andrei");
         System.out.println("Hi Andrei!");
     }
 }
---
+++ src/main/java/git/ClassB.java
@@ -0,0 +1,9 @@
+package git;
+
+public class ClassB {
+
+    public void sayHi(String name){
+        System.out.println("Hi" + name + "");
+    }
+
+}
