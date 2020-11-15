21bf7155d5789f958f24e146fff348d4e255dd2f
iulian <iuliandiaconescu86@gmail.com>
2020-11-15 12:37:34 +0200

    git:call method in ClassA

--- src/main/java/git/ClassA.java
+++ src/main/java/git/ClassA.java
@@ -3,5 +3,8 @@ package git;
 public class ClassA {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        ClassB classB = new ClassB();
+        classB.sayHi();
     }
 }
